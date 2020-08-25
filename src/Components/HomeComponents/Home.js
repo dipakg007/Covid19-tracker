fdb8b4fdb7a599e46e4c18d1b81e3a6e9b1fd604
Dipak Gupta <dipakg286@gmail.com>
2020-08-25 17:50:24 +0530

    Home page updates

--- src/Components/HomeComponents/Home.js
+++ src/Components/HomeComponents/Home.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import logo from "./images/graph.png";
 import globe from "./images/globe.png";
+import newcase from "./images/virus.png";
+import newrecover from "./images/heartbeat.png";
+import newdeaths from "./images/death.png";
 import "./Home.css";
 import { FormControl, Select, MenuItem, Avatar } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import { prettyPrintStat } from "./util";
+import numeral from "numeral";
 
 function Home() {
   const [countries, setCountries] = useState(["India", "Usa", "UK"]);
-  const [country, setCountry] = useState("worldwide");
+  const [country, setCountry] = useState("Worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -25,9 +33,27 @@ function Home() {
     getCountriesData();
   }, []);
 
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
+    console.log(countryCode);
+    const url =
+      countryCode === "Worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
 
   return (
@@ -46,7 +72,7 @@ function Home() {
                   onChange={onCountryChange}
                   value={country}
                 >
-                  <MenuItem value="worldwide">
+                  <MenuItem value="Worldwide">
                     <div className="homedrop__item">
                       <Avatar alt="" src={globe} />
                       <h5>Worldwide</h5>
@@ -64,6 +90,36 @@ function Home() {
               </FormControl>
             </div>
           </div>
+
+          <div className="home__stats">
+            <InfoBox
+              title={`Today's ${
+                country === "Worldwide" ? country : countryInfo.country
+              } New Cases`}
+              cases={numeral(countryInfo.todayCases).format("0,0")}
+              total={prettyPrintStat(countryInfo.cases)}
+              image={newcase}
+            />
+
+            <InfoBox
+              title={`Today's ${
+                country === "Worldwide" ? country : countryInfo.country
+              } Recovered Cases`}
+              cases={numeral(countryInfo.todayRecovered).format("0,0")}
+              total={prettyPrintStat(countryInfo.recovered)}
+              image={newrecover}
+            />
+
+            <InfoBox
+              title={`Today's ${
+                country === "Worldwide" ? country : countryInfo.country
+              } Deaths Cases`}
+              cases={numeral(countryInfo.todayDeaths).format("0,0")}
+              total={prettyPrintStat(countryInfo.deaths)}
+              image={newdeaths}
+            />
+          </div>
+          <Map />
         </div>
       </div>
     </div>
---
+++ src/Components/HomeComponents/InfoBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./InfoBox.css";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({ title, cases, total, image }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <div className="infoBox__main">
+          <div className="infoBox__left">
+            <Typography className="infoBox__title" color="textSecondary">
+              {title}
+            </Typography>
+            <h2 className="infoBox__cases">+{cases}</h2>
+            <Typography className="infoBox__total">{total} Total</Typography>
+          </div>
+          <div className="infoBox__right">
+            <img className="infoBox__logo" src={image}></img>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
