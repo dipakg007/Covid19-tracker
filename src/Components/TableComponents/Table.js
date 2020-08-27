930f49fe8b1ec0850af583f922c222228e506740
sachin <koolsachinkumar82@gmail.com>
2020-08-27 12:20:27 +0530

    Navbar  fixed

--- src/Components/TableComponents/Table.js
+++ src/Components/TableComponents/Table.js
@@ -1,9 +1,10 @@
 import React from "react";
 import DataApi from './DataApi';
+import './Table.css';
 
 function Table() {
   return (
-    <div className="container-fluid nav_bg">
+    <div className="container-fluid nav_bg main_table">
       <div className="row">
         <div className="col-11 mx-auto">
           <h1>Covid-tracking Country wise</h1>
