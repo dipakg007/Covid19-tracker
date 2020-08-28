59070a807b1d5f691bbc1baf4dc89274faf0b009
Dipak Gupta <dipakg286@gmail.com>
2020-08-28 11:20:28 +0530

    Day4

--- src/Components/HomeComponents/InfoBox.js
+++ src/Components/HomeComponents/InfoBox.js
@@ -2,9 +2,24 @@ import React from "react";
 import "./InfoBox.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({ title, cases, total, image }) {
+function InfoBox({
+  title,
+  active,
+  cases,
+  total,
+  image,
+  isCases,
+  isRecovered,
+  isDeaths,
+  ...props
+}) {
   return (
-    <Card className="infoBox">
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isCases && "infoBox--cases"
+      }`}
+    >
       <CardContent>
         <div className="infoBox__main">
           <div className="infoBox__left">
